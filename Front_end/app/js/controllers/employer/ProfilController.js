612694756580a541f7f1b5e25ff9d6e514d6dbc9
ibtahiri <itahiri@ymagisgroup.com>
2018-12-18 12:43:15 +0000

    translate

--- Front_end/app/js/controllers/employer/ProfilController.js
+++ Front_end/app/js/controllers/employer/ProfilController.js
@@ -5,6 +5,7 @@
 			function($scope,$rootScope,$http,EmployersDatasrv,$window,$interval) {
 		
 	   $rootScope.employerProfil=$rootScope.user;
+	   //la date de recrutement
 		var date= $rootScope.employerProfil.dateEntry;
 		 var d = new Date(date);
 			$scope.day=d.getDate();
@@ -12,16 +13,31 @@
 			$scope.year=d.getFullYear();
 	        $scope.dateStart=$scope.day+"/"+$scope.month+"/"+$scope.year;	
 		  
-			
-		EmployersDatasrv.editEmployer( $rootScope.employerProfil.idEmp)
+		//get user's informations	
+		EmployersDatasrv.editEmployer($rootScope.employerProfil.idEmp)
 		.then(function (data) {
 			$rootScope.employerProfil= data.data;
-			$scope.first= $rootScope.employerProfil.firstName;
-			$scope.last= $rootScope.employerProfil.lastName;
+			$rootScope.first= $rootScope.employerProfil.firstName;
+			$rootScope.last= $rootScope.employerProfil.lastName;
  			}, function (err) {
  				console.log(err);
  			})
 		
+		 //get Managers List
+		 EmployersDatasrv.getManagersList()
+		 .then(function (data) {
+			 $scope.managerList= data;
+			 //get employer manager
+			 for(var i=0;i<$scope.managerList.length;i++){
+				 for(var j=0;j<$scope.managerList[i].managerTeam.length;j++){
+					 if($scope.managerList[i].managerTeam[j].idEmp==$rootScope.employerProfil.idEmp){
+						$rootScope.manager=$scope.managerList[i]
+						$rootScope.managerName=$rootScope.manager.firstName+" "+$rootScope.manager.lastName;
+						 
+					 }
+				 }
+			 }
+		 })
 
 		
 		
